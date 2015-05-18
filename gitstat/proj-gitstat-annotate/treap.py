a5e4e4a11083041c1414866f130eff51a7ab5fbb
LDVSOFT <lapshin.dv@mail.ru>
2015-05-18 10:54:38 +0300

    #6: Finally done

--- gitstat/proj-gitstat-annotate/treap.py
+++ gitstat/proj-gitstat-annotate/treap.py
@@ -1,31 +1,37 @@
 from random import random
 
+
 class Treap:
-    def __init__(self, value):
+    def __init__(self, value=0):
         self.value = value
         self.y = random()
-        self.cnt = 0
-        self.toAdd = 0
+        self.id = 1
+        self.size = 1
         self.left = self.right = None
 
-    def Push(self):
-        if not self.left is None:
-            self.left.toAdd += self.toAdd
-        if not self.right is None:
-            self.right.toAdd += self.toAdd
-        self.cnt += self.toAdd
-        self.toAdd = 0
+    def Normalize(self):
+        self.id = self.size = 1 + Size(self.left)
+        self.size += Size(self.right)
+
+
+def Size(node):
+    if node is None:
+        return 0
+    return node.size
+
 
 def Split(node, x):
     if node is None:
         return None, None
-    node.Push()
-    if node.x <= x:
-        node.right, b = Split(node.right, x)
+    if node.id <= x:
+        node.right, b = Split(node.right, x - node.id)
+        node.Normalize()
         return node, b
     else:
         a, node.left = Split(node.left, x)
+        node.Normalize()
         return a, node
+
 
 def Merge(left, right):
     if left is None:
@@ -34,22 +40,39 @@ def Merge(left, right):
         return left
     if left.y < right.y:
         left.right = Merge(left.right, right)
+        left.Normalize()
         return left
     else:
         right.left = Merge(left, right.left)
+        right.Normalize()
         return right
+
 
 def Build(n):
     root = None
     for i in range(1, n + 1):
-        Merge(root, Treap(i))
+        root = Merge(root, Treap())
     return root
 
-def Out(root):
-    i = 1
-    while not root is None:
-        v, root = Split(root, i)
-        i += 1
-        print("%d: %d" % (i, v.cnt))
-    
 
+def Debug(root, level = 1):
+    if root is None:
+        return
+    Debug(root.right, level + 1)
+    print("!", "    " * level, "%d (id = %d size = %d)" % (root.value, root.id, root.size), sep="")
+    Debug(root.left,  level + 1)
+
+
+def Out(root, file=None):
+    i = 1
+    isEmpty = False
+    while not root is None:
+        v, root = Split(root, 1)
+        if (v.value):
+            print("%5d: %5d %s" % (i, v.value, file[i - 1] if not file is None else ""))
+            isEmpty = False
+        else:
+            if not isEmpty:
+                print("...")
+            isEmpty = True
+        i += 1
